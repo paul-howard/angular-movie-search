d148cda1fb97bc6666fba793cd5367f98f9ddc1d
Paul Howard <paul@paul-howard.net>
2015-05-21 07:40:00 -0700

    Add basic search function

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var moviesApp = angular.module('moviesApp', []);
 
-moviesApp.controller('searchController', ['$scope', function($scope){
+moviesApp.controller('searchController', ['$scope', '$http', function($scope, $http) {
+
   $scope.searchTerm = '';
-}])
+
+  // * Main Search Function
+  $scope.search = function() {
+
+    var req = {
+      url: 'http://www.omdbapi.com',
+      params: {
+        s:    $scope.searchTerm
+      }
+    };
+
+    $http(req).success(function(data) {
+      $scope.movies = data;
+    });
+
+  };
+
+}]);
