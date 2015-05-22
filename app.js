84ad4b753944cf414f57ca98c9e7ae82ffcf5c99
Paul Howard <paul@paul-howard.net>
2015-05-22 09:52:41 -0700

    Remove leading slashes on 'templateUrl' values

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ moviesApp.config(function($routeProvider) {
 
   $routeProvider
     .when('/', {
-      templateUrl: '/pages/results.html',
+      templateUrl: 'pages/results.html',
       controller: 'searchController'
     })
     .when('/details/:id', {
-      templateUrl: '/pages/details.html',
+      templateUrl: 'pages/details.html',
       controller: 'detailsController'
     });
 
